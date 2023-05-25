b6b539c7fdeb0ccbc51ce65f28acd3b5ed973f18
Carl Grundberg <carl.grundberg@menmo.se>
2023-05-25 20:43:00 +0200

    Info

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,51 @@
 import Layout from "../components/Layout";
 import SignUp from "../components/SignUp";
-import { Row, Col } from "antd";
+import { Row, Col, Button } from "antd";
 
 export default function Home() {
   return (
     <Layout>
       <h2>Lördagen den 16 september 2023 är det åter dags för cykelfest!</h2>
       <p>
-        Vi kommer senare i vår skicka ut inbjudan men skriv gärna upp datumet
-        redan nu så att ni inte bokar in något annat.
+        Efter fantastiska maträtter, god dricka, vingliga cykelturer och
+        eventuella besök i buskar och diken träffas vi för att festa natten
+        lång.
       </p>
       <p>
-        Vittsjö cykelfest är till för alla som bor i Vittsjö. Festen anordnas
-        varannat år på hösten, och går ut på att man som par får i uppgift att
-        laga en rätt (förrätt, huvudrätt eller efterrätt). Vilka som äter rätten
-        ihop slumpas fram och ni kommer både få besök och besöka okända och
-        kända Vittsjöbor.
+        Ni kommer att arrangera en förrätt, varmrätt eller en dessert för 6-8
+        personer inklusive er själva hemma hos er. Vilka som kommer till er
+        avgör slumpen. Vart ni själva ska äta de andra två rätterna är en
+        överraskning. Om ni har någon form av allergi eller annan överkänslighet
+        så meddela detta till festfixarna i samband med anmälan.
       </p>
-      <SignUp />
+      <p>
+        I god tid innan festen får ni instruktioner om vilken måltid ni bjuder
+        på. Övriga instruktioner erhåller ni på festdagen. Vi startar med
+        gemensam samling och kvällen avslutas med fest och dans på Stickspåret i
+        Vittsjö.
+      </p>
+      <p>Kalaset kostar 250:-/par</p>
+      <p>
+        Anmäl er senast 30 juni genom att fylla i anmälningsformuläret. Swisha
+        sedan till 076-119 41 13 (Ingela Lundqvist) för att slutföra anmälan.
+        Anmälan är bindande!
+      </p>
+
+      <p>
+        <Button
+          href="https://vittsjo.cykelfest.online/signup"
+          type="primary"
+          size="large"
+          block
+        >
+          Till anmälan
+        </Button>
+      </p>
+
+      <p>
+        Obs! Information kommer från e-post info@cykelfest.online så om du inte
+        får något mail så kontrollera om det hamnat i skräpposten.
+      </p>
     </Layout>
   );
 }
